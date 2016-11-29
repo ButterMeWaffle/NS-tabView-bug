5d78e5910f1a59109a706521c808bdba406bcad8
Samuel Reeder <sam@webfab.co>
2016-11-29 11:41:55 -0500

    firt commit

---
+++ my-app-name/platforms/android/build/intermediates/assets/F0/debug/app/app.component.js
@@ -0,0 +1,49 @@
+"use strict";
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+        this.counter = 16;
+    }
+    Object.defineProperty(AppComponent.prototype, "message", {
+        get: function () {
+            if (this.counter > 0) {
+                return this.counter + " taps left";
+            }
+            else {
+                return "Hoorraaay! \nYou are ready to start building!";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AppComponent.prototype.onTap = function () {
+        this.counter--;
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: "my-app",
+            templateUrl: "app.component.html",
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+var TestComponent = (function () {
+    function TestComponent() {
+        this.items = new Array();
+        for (var i = 0; i < 5; i++) {
+            this.items.push(new DataItem("item " + i));
+        }
+    }
+    return TestComponent;
+}());
+exports.TestComponent = TestComponent;
+var DataItem = (function () {
+    function DataItem(itemDesc) {
+        this.itemDesc = itemDesc;
+    }
+    return DataItem;
+}());
+exports.DataItem = DataItem;
+//# sourceMappingURL=app.component.js.map
